4bb54fc77e8a70a8a61254b0c053abf2bc560fb3
Benjamin Barbier <benjamin.barbier@dark-sides.com>
2013-06-23 20:17:13 +0200

    Display menu on list

--- menu.js
+++ menu.js
@@ -1,32 +1,30 @@
 function MainCtrl($scope) {
 
-$scope.menu = [
-  {"title" : "Applications", "menu" : [
-  	{"title" : "Integration", "menu" : [
-      {"title" : "Application 1", "link" : "#application1-int"},
-      {"title" : "Application 2", "link" : "#application2-int"}
+  $scope.menu = [
+    {"title" : "Applications", "menu" : [
+      {"title" : "Integration", "menu" : [
+        {"title" : "Application 1", "link" : "#application1-int"},
+        {"title" : "Application 2", "link" : "#application2-int"}
+      ]},
+      {"title" : "Pre-Production", "menu" : [
+        {"title" : "Application 1", "link" : "#application1-preprod"},
+        {"title" : "Application 2", "link" : "#application2-preprod"}
+      ]},
+      {"title" : "Production", "menu" : [
+        {"title" : "Application 1", "link" : "#application1-prod"},
+        {"title" : "Application 2", "link" : "#application2-prod"}
+      ]}
     ]},
-  	{"title" : "Pre-Production", "menu" : [
-      {"title" : "Application 1", "link" : "#application1-preprod"},
-      {"title" : "Application 2", "link" : "#application2-preprod"}
+    {"title" : "Tools", "menu" : [
+      {"title" : "Tool 1", "link" : "#tool1"},
+      {"title" : "Tool 2", "link" : "#tool2"},
+      {"title" : "Tool 3", "link" : "#tool3"},
+      {"title" : "Tool 4", "link" : "#tool4"}
     ]},
-  	{"title" : "Production", "menu" : [
-      {"title" : "Application 1", "link" : "#application1-prod"},
-      {"title" : "Application 2", "link" : "#application2-prod"}
+    {"title" : "Folders", "menu" : [
+      {"title" : "Folder 1", "link" : "#folder1"},
+      {"title" : "Folder 2", "link" : "#folder2"}
     ]}
-  ]},
-  {"title" : "Tools", "menu" : [
-    {"title" : "Tool 1", "link" : "#tool1"},
-    {"title" : "Tool 2", "link" : "#tool2"},
-    {"title" : "Tool 3", "link" : "#tool3"},
-    {"title" : "Tool 4", "link" : "#tool4"}
-  ]},
-  {"title" : "Folders", "menu" : [
-    {"title" : "Folder 1", "link" : "#folder1"},
-    {"title" : "Folder 2", "link" : "#folder2"}
-  ]}
-];
-
-$scope.m = "toto";
+  ];
 
 }
