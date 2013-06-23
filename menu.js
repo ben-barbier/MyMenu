5cb382e87b021e840fe6c8812bea582a7dd12342
Benjamin Barbier <benjamin.barbier@dark-sides.com>
2013-06-24 01:27:00 +0200

    Use new formatMenu functions

--- menu.js
+++ menu.js
@@ -1,26 +1,6 @@
-angular.module('MyMenu', [])
-    .filter('formatMenuElement', function () {
-        return function (text) {
-
-          var parts = text.split(" : ");
-          var title = parts[0];
-          var elt = "";
-
-          if (parts.length == 1) {
-            elt += title;
-          }
-          if (parts.length == 2) {
-            var url = parts[1];
-            elt += "<a href=\"" + url + "\">" + title + "</a>";
-          }
-          
-          return elt;
-        };
-    });
 
 function MainCtrl($scope) {
 
-  $scope.deep = 0;
   $scope.menu = [
     "Applications",
     "- Integration",
@@ -41,5 +21,64 @@ function MainCtrl($scope) {
     "- Folder 1 : #folder1",
     "- Folder 2 : #folder2"
   ];
+
+  $scope.formatMenu = function(menu) {
+
+    var result = "";
+    var previousDeep = 0;
+
+    result += "<ul>";
+    for(var i=0;i<menu.length;i++) {
+      deep = computeDeep(menu[i]);
+      if (deep>previousDeep) {result += "<ul>";}
+      if (deep<previousDeep) {result += "</ul>";}
+      result += formatMenuElement(menu[i], deep);
+      previousDeep = deep;
+    }
+    result += "</ul>";
+
+    return result;
+  };
+
+  var computeDeep = function(elt) {
+  	var deep = 0;
+  	for(var i=0;i<elt.length;i++) {
+      if (elt[i]=="-"){
+      	deep++;
+      } else {
+      	break;
+      }
+  	}
+  	return deep;
+  }
+
+  var formatMenuElement = function(menuElement, deep) {
+
+    var parts = menuElement.split(" : ");
+    var title = parts[0];
+    var elt = "";
+
+    title = formatTitle(title);
+
+    elt += "<li>";
+    if (parts.length == 1) {
+      elt += title;
+    }
+    if (parts.length == 2) {
+      var url = parts[1];
+      elt += "<a href=\"" + url + "\">" + title + "</a>";
+    }
+    elt += "</li>";
+    return elt;
+  }
+
+  var formatTitle = function(title) {
+    for(var i=0;i<title.length;i++) {
+      if (title[i] == "-" || title[i] == " ") {
+      	title = title.slice(1);
+      }
+    }
+  	return title;
+  }
 
 }
